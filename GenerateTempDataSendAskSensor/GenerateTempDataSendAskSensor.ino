2b0bf29b413dc7b2e756b166b347990e3e29f2c6
TomDeBat <masodebat@hotmail.com>
2022-10-11 22:35:20 +1100

    Added wificonfig settings for testing

--- GenerateTempDataSendAskSensor/GenerateTempDataSendAskSensor.ino
+++ GenerateTempDataSendAskSensor/GenerateTempDataSendAskSensor.ino
@@ -45,17 +45,36 @@ Serial.println("*****************************************************");
 Serial.println("********** Program Start : Connect ESP32 to AskSensors.");
 Serial.println("Wait for WiFi... ");
 sensors.begin();
-// connecting to the WiFi network
-WiFiMulti.addAP(ssid, password);
-while (WiFiMulti.run() != WL_CONNECTED) {
-Serial.print("WiFi not connected......");
+
+//Init WiFi as Station, start SmartConfig
+WiFi.mode(WIFI_AP_STA);
+WiFi.beginSmartConfig();
+
+//Wait for SmartConfig packet from mobile
+Serial.println("Waiting for SmartConfig.");
+while (!WiFi.smartConfigDone()) {
 delay(500);
+Serial.print(".");
 }
-// connected
-Serial.println("WiFi connected");
-Serial.println("IP address: ");
+
+Serial.println("");
+Serial.println("SmartConfig received.");
+
+//Wait for WiFi to connect to AP
+Serial.println("Waiting for WiFi");
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+
+Serial.println("WiFi Connected.");
+
+Serial.print("IP Address: ");
 Serial.println(WiFi.localIP());
+
 }
+
+
 void loop(){
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
